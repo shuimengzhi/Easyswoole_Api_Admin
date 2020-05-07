8e7e7e5b46c490e380cb1631ea45ff2fe1191343
shuimengzhi <shui_jingshan@163.com>
2020-05-07 16:58:25 +0800

    全局设置跨域头

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -5,6 +5,7 @@
 
     use EasySwoole\EasySwoole\Swoole\EventRegister;
     use EasySwoole\EasySwoole\AbstractInterface\Event;
+    use EasySwoole\Http\Message\Status;
     use EasySwoole\Http\Request;
     use EasySwoole\Http\Response;
     use EasySwoole\Component\Di;
@@ -19,7 +20,7 @@
         {
             // TODO: Implement initialize() method.
             date_default_timezone_set('Asia/Shanghai');
-            $config=new OrmConfig(Config::getInstance()->getConf('MYSQL'));
+            $config = new OrmConfig(Config::getInstance()->getConf('MYSQL'));
             DbManager::getInstance()->addConnection(new Connection($config));
 
 //        允许 URL 最大解析至7层
@@ -34,6 +35,15 @@
         public static function onRequest(Request $request, Response $response): bool
         {
             // TODO: Implement onRequest() method.
+            $url = Config::getInstance()->getConf('FRONT_END_URL');
+            $response->withHeader('Access-Control-Allow-Origin', $url);
+            $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+            $response->withHeader('Access-Control-Allow-Credentials', 'true');
+            $response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            if ($request->getMethod() === 'OPTIONS') {
+                $response->withStatus(Status::CODE_OK);
+                return false;
+            }
             return true;
         }
 
