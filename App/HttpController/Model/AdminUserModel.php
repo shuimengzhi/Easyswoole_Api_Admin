9e016d79a0b502db726431aa55494e3003e5191b
shuimengzhi <shui_jingshan@163.com>
2020-05-07 14:59:52 +0800

    管理员模型添加TOKEN字段

--- App/HttpController/Model/AdminUserModel.php
+++ App/HttpController/Model/AdminUserModel.php
@@ -16,6 +16,10 @@
          * @var string
          */
         protected $tableName = 'admin_user';
+        private $admin_name;
+        private $password;
+        private $email;
+        private $create_time;
 
         /**
          * @param bool $isCache
@@ -30,16 +34,18 @@
                 ->setColumnComment('后台管理员名称');
             $table->colVarChar('password', 60)->setIsNotNull()
                 ->setColumnComment('密码');
-            $table->colVarChar('email',60)->setIsNotNull()
+            $table->colVarChar('email', 60)->setIsNotNull()
                 ->setColumnComment('Email');
             $table->colInt('create_time', 30)->setIsNotNull()
                 ->setColumnComment('注册时间');
             $table->colInt('last_time', 30)
                 ->setColumnComment('最后登录时间');
-            $table->colVarChar('last_ip',30)
+            $table->colVarChar('last_ip', 30)
                 ->setColumnComment('最后登录的IP地址');
             $table->colText('action_list')->setIsNotNull()
                 ->setColumnComment('授权的功能权限');
+            $table->colVarChar('token',350)->setIsUnique()
+                ->setColumnComment('token');
             return $table;
         }
     }
